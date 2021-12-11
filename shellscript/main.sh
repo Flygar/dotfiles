bea1d63cb4c3b6446e5b5370c9ed6aff40a25dd7
Flygar <w871546712@gmail.com>
2021-12-12 02:38:56 +0800

    Update main.sh

--- shellscript/main.sh
+++ shellscript/main.sh
@@ -161,6 +161,8 @@ function main() {
     wait4done "update_ssh_port "${NEW_PORT}"" "${COLOR_SUCC}>>>update_ssh_port${COLOR_NONE}" && restart_sshd
     
     echo "pwt: ${NEW_USER_PASSWD}"
+    
+    NEW_USER_PASSWD=$(echo ${NEW_USER_PASSWD} ｜ sed 's/\$/\\$/g')
 
     # 添加新用户
     wait4done "add1user "${NEW_USER}" ${NEW_USER_PASSWD}" "${COLOR_SUCC}>>>adduser ${NEW_USER}${COLOR_NONE}"
